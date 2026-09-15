40784353970584325a4b76474864e56856c82d5d
안정규 <ahnjunggyu1114@gmail.com>
2026-09-16 02:46:22 +0900

    [Feat] 네브바 태그 필터 상태 표시

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,19 +2,47 @@ import Plus from '../assets/Plus.svg';
 import Search from '../assets/Search.svg';
 import Profile from '../assets/Profile.svg';
 import Tag from '../assets/Tag.svg';
+import { useState } from 'react';
 
 function Navbar() {
+  const [isTagOpen, setIsTagOpen] = useState(false);
+
   return (
     <nav className="flex h-[80px] w-full items-center gap-[16px]">
       {/* 왼쪽 검색 영역 */}
       <div className="flex h-full flex-1 items-center gap-[12px] rounded-[28px] bg-[#FAFAFA] p-[16px]">
-        <button className="flex h-[48px] cursor-pointer shrink-0 items-center gap-[8px] rounded-[36px] bg-[#E4EDFF] px-[16px]">
-          <span className=" text-[16px] leading-[24px] font-extrabold text-[#001B51]">
-            태그 선택
-          </span>
+        <div className="relative shrink-0">
+          <button
+            className="flex h-[48px] cursor-pointer shrink-0 items-center gap-[8px] rounded-[36px] bg-[#E4EDFF] px-[16px]"
+            onClick={() => setIsTagOpen(!isTagOpen)}
+          >
+            <span className=" text-[16px] leading-[24px] font-extrabold text-[#001B51]">
+              태그 선택
+            </span>
 
-          <img src={Tag} alt="" />
-        </button>
+            <img src={Tag} alt="" />
+          </button>
+
+          {isTagOpen && (
+            <div className="absolute top-0 left-full ml-[10px] flex w-[116px] flex-col overflow-hidden rounded-[16px] bg-[#E4EDFF] ">
+              <button className="cursor-pointer px-[16px] py-[12px] text-left">
+                전체보기
+              </button>
+
+              <button className="cursor-pointer px-[16px] py-[12px] text-left text-[#7BA7FF]">
+                Daily
+              </button>
+
+              <button className="cursor-pointer px-[16px] py-[12px] text-left text-[#0037A3]">
+                Work
+              </button>
+
+              <button className="cursor-pointer px-[16px] py-[12px] text-left text-[#A6B7CB]">
+                Others
+              </button>
+            </div>
+          )}
+        </div>
 
         <input
           type="text"
