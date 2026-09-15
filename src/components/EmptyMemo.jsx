38583e2544ef639391d72f09213f5f527a441233
안정규 <ahnjunggyu1114@gmail.com>
2026-09-16 01:00:29 +0900

    [Feat] 빈 메모장 구현

---
+++ src/components/EmptyMemo.jsx
@@ -0,0 +1,23 @@
+import PlusWhite from '../assets/Plus_white.svg';
+
+function EmptyMemo() {
+  return (
+    <section className="flex h-[710px] items-center justify-center rounded-[24px] border-2 border-dashed border-[#AAC8FF]">
+      <div className="flex flex-col items-center gap-[28px]">
+        <button
+          type="button"
+          className="flex h-[120px] w-[120px] items-center justify-center rounded-[75px] bg-[#AAC8FF]"
+          aria-label="새 메모 작성"
+        >
+          <img src={PlusWhite} alt="" />
+        </button>
+
+        <p className="text-[24px] leading-[32px] font-semibold text-[#AAC8FF]">
+          새로운 메모를 작성해보세요!
+        </p>
+      </div>
+    </section>
+  );
+}
+
+export default EmptyMemo;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,15 @@
-import Navbar from '../components/NavBar';
+import NavBar from '../components/NavBar';
+import EmptyMemo from '../components/EmptyMemo';
 
 const Home = () => {
   return (
     <main className="min-h-screen w-full bg-[#E4EDFF] px-[120px] pt-[72px] pb-[86px]">
       <div className="flex  w-full flex-col gap-[76px]">
-        <Navbar />
+        {/* NavBar */}
+        <NavBar />
 
         {/* Memo Content */}
-        <div className="flex-1 border border-dashed border-[#9CC1FF]">
-          Memo Content
-        </div>
+        <EmptyMemo />
       </div>
     </main>
   );
